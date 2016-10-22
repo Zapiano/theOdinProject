049fb494470b032b0bef900a10755f1b908dd6e9
Zapiano <2pedro8@gmail.com>
2016-10-22 13:52:23 -0200

    Add bubble_sort and bubble_sort_by methods
    Bubble sort just sorts an array comparing the elements. Bubble sort by
    lets you pass a block specifying the comparison method.

---
+++ ruby programming/advanced_building_blocks/bubble_sort.rb
@@ -0,0 +1,43 @@
+# 1. se o array tem n elementos, é preciso fazer no máximo n-1 iterações
+# 2. a cada iteração i só é preciso fazer n - i comparações
+#
+# ex:n = 4
+# (4,3,2,1)
+# i = 1 -> (3,2,1,4) 3 comparações
+# i = 2 -> (2,1,3,4) 2 comparações
+# i = 3 -> (1,2,3,4) 1 comparação
+
+def bubble_sort(array)
+  array_size = array.length
+  number_of_iterations = array_size - 1
+
+  for iteration in (1..number_of_iterations)
+    number_of_comparisons = array_size - iteration
+
+    for element in (1..number_of_comparisons)
+      if array[element - 1] > array[element]
+        array[element - 1], array[element] = array[element], array[element - 1]
+      end
+    end
+  end
+
+  return array
+end
+
+
+def bubble_sort_by(array)
+  array_size = array.length
+  number_of_iterations = array_size - 1
+
+  for iteration in (1..number_of_iterations)
+    number_of_comparisons = array_size - iteration
+
+    for element in (1..number_of_comparisons)
+      if yield(array[element - 1], array[element])
+        array[element - 1], array[element] = array[element], array[element - 1]
+      end
+    end
+  end
+
+  return array
+end
